d04c29b5053f7c708cfd47975fd4f6e9fbee7d04
silavsale <silavsale@gmail.com>
2020-07-21 22:06:15 -0400

    add redux to app

---
+++ src/reducers/todoReducer.js
@@ -0,0 +1,11 @@
+const initState = {
+    todos: [],
+    todo: ''
+}
+
+export default(state = initState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
